23911a2566eaa103f83852d0a55c42e74097c64a
skeeu <bisembaev.arman@gmail.com>
2023-10-22 17:50:46 +0600

    fix adapter pattern

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        HalykBankProcessor halykBankProcessor = HalykBankProcessor.getInstance();
+        KaspiProcessor kaspiProcessor = new KaspiProcessor();
+        HalykBankAdapter halykBankAdapter = new HalykBankAdapter();
 
         PaymentApplication paymentApplication = new PaymentApplication();
 
-        paymentApplication.performPayment(halykBankProcessor, "21312fsdf", 123, 111, "some info");
+        paymentApplication.performPayment(kaspiProcessor, "21312fsdf", 123, 111, "some info");
+        paymentApplication.performPayment(halykBankAdapter, "21312fsdf", 123, 111, "some info");
     }
 }
